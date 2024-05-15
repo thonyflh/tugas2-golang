9bd96676b1f3dc1fb1780fa6b1278ea55a3774f6
rizky.alfariz@lumoshive.com <anthonyflh16@gmail.com>
2024-05-15 11:06:47 +0700

    kedua

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"encoding/xml"
 	"net/http"
 	"sesi-3/app"
 )
@@ -23,7 +24,53 @@ func productsHandler(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(products)
 }
 
+func ProductHandlerXML(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/xml")
+
+	var products []app.Product
+
+	ecommerceCategory := app.Category{}
+	ecommerceCategory.SetIDAndName(2, "Ecommerce")
+
+	projectManagementCategory := app.Category{}
+	projectManagementCategory.SetIDAndName(1, "Project management")
+
+	product1 := app.NewProduct(1, "Lakugan", 1000000000, 2023, ecommerceCategory)
+	product2 := app.NewProduct(2, "My score card", 2000000000, 2024, projectManagementCategory)
+	products = append(products, *product1, *product2)
+
+	xmlData, err := xml.MarshalIndent(products, "", "    ")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Write([]byte(xml.Header))
+	w.Write(xmlData)
+}
+
+func AddProductHandlerJSON(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	var newProduct app.Product
+	err := json.NewDecoder(r.Body).Decode(&newProduct)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	responseData, err := json.Marshal(newProduct)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Write(responseData)
+}
+
 func main() {
 	http.HandleFunc("/api/v1/products.json", productsHandler)
+	http.HandleFunc("/api/v1/products.xml", ProductHandlerXML)
+	http.HandleFunc("/api/v1/add-products.json", AddProductHandlerJSON)
 	http.ListenAndServe(":8081", nil)
 }
